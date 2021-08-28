9c84a07184613f8ac2fd8d85198bb39b41cd9f70
Tricketteg <tricketteh@gmail.com>
2021-08-28 15:34:31 +0300

    TTB-0: added SpringBoot skeleton for project

---
+++ src/main/java/com/github/Tricketteh/ttb/TricheTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.Tricketteh.ttb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricheTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TricheTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Tricketteh/ttb/TricheTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Tricketteh.ttb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricheTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
